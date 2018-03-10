17fe4071f5a1f7b73a28c8871aa60f4821e6d84f
Mukudori9316 <ryotaf1116@gmail.com>
2018-03-10 23:07:57 +0900

    Add odds_table and define new Item

--- keirin_scraper/items.py
+++ keirin_scraper/items.py
@@ -12,3 +12,13 @@ class KeirinScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     race_id = scrapy.Field()
+    player1 = scrapy.Field()
+    player2 = scrapy.Field()
+    player3 = scrapy.Field()
+    player4 = scrapy.Field()
+    player5 = scrapy.Field()
+    player6 = scrapy.Field()
+    player7 = scrapy.Field()
+    player8 = scrapy.Field()
+    player9 = scrapy.Field()
+    odds_table = scrapy.Field()
--- keirin_scraper/spiders/webspider.py
+++ keirin_scraper/spiders/webspider.py
@@ -2,8 +2,10 @@
 import scrapy
 import re
 from keirin_scraper.items import KeirinScraperItem
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
+import numpy as np
+import pandas as pd
 
 
 class KeirinSpider(scrapy.Spider):
@@ -13,24 +15,29 @@ class KeirinSpider(scrapy.Spider):
     rules = (
         # データ抽出ルール
         Rule(LinkExtractor(
-            unique=True # おなじリンク先ではデータ抽出しない
-            ),
+            unique=True     # おなじリンク先ではデータ抽出しない
+        ),
         ),
     )
 
     def parse(self, response):
-        for href in response.css('.kaisai-program_table .result a::attr(href)'):
+        for href in response.css(
+                '.kaisai-program_table .result a::attr(href)'):
             full_url = response.urljoin(href.extract())
-            urls.append(full_url)
-            race_id = re.findall(r'\d{16}', full_url)[0]
-            race_ids.append(race_id)
+            # urls.append(full_url)
+            # race_id = re.findall(r'\d{16}', full_url)[0]
+            # race_ids.append(race_id)
         yield scrapy.Request(full_url, callback=self.parse_race)
 
-        # follow_link = response.css('.raceinfo-date_nav-next a::attr(href)')
-        # follow_url = response.urljoin(follow_link.extract_first())
-        # yield scrapy.Request(follow_url, callback=self.parse)
-        
+        follow_link = response.css('.raceinfo-date_nav-next a::attr(href)')
+        follow_url = response.urljoin(follow_link.extract_first())
+        yield scrapy.Request(follow_url, callback=self.parse)
+
     def parse_race(self, response):
+        item = KeirinScraperItem()
+        url = response.url
+        race_id = re.findall(r'\d{16}', url)[0]
+        item['race_id'] = race_id
         players = []
         # 1 出走表 2 前回出走成績 3 今場所成績
         players1, players2, players3 = response.css('table.racecard_table')
@@ -42,29 +49,60 @@ class KeirinSpider(scrapy.Spider):
         players = []
         for player in tmp:
             p = re.compile(r"<[^>]*?>")
-            datas = [re.split('[\r\n\t/]', p.sub("", data).strip()) for data in player.css('td').extract()]
+            datas = [
+                re.split(
+                    '[\r\n\t/]',
+                    p.sub("", data).strip().replace(" ", "").replace("\u3000", "")
+                )
+                for data
+                in player.css('td').extract()
+            ]
             pdic = {
-                'bike_num': datas[0][0],
+                'bike_num': int(datas[0][0]),
                 'name': datas[1][0],
                 'hometown': datas[1][-3],
                 'age': datas[1][-2],
-                'year': datas[1][-1],
+                'year': int(datas[1][-1]),
                 'rank': datas[2][0],
                 'feet': datas[3][0],
-                'gear': datas[4][0],
-                'point': datas[5][0],
-                'S': datas[6][0],
-                'B': datas[7][0],
-                'nige': datas[8][0],
-                'makuri': datas[9][0],
-                'sashi': datas[10][0],
-                'mark': datas[11][0],
-                'first': datas[12][0],
-                'second': datas[13][0],
-                'third': datas[14][0],
-                'over_rank': datas[15][0],
-                'win_rate': datas[16][0],
-                'in_second_rate': datas[16][0],
-                'in_third_rate': datas[16][0],
+                'gear': float(datas[4][0]),
+                'point': float(datas[5][0]),
+                'S': int(datas[6][0]),
+                'B': int(datas[7][0]),
+                'nige': int(datas[8][0]),
+                'makuri': int(datas[9][0]),
+                'sashi': int(datas[10][0]),
+                'mark': int(datas[11][0]),
+                'first': int(datas[12][0]),
+                'second': int(datas[13][0]),
+                'third': int(datas[14][0]),
+                'over_rank': int(datas[15][0]),
+                'win_rate': float(datas[16][0]),
+                'in_second_rate': float(datas[16][0]),
+                'in_third_rate': float(datas[16][0]),
             }
+            item['player{}'.format(pdic['bike_num'])] = pdic
             players.append(pdic)
+        # 3連単とオッズのイテレータ
+        odds_num = response.css('li tr span.num::text').extract()
+        odds = response.css('li tr span.odds::text').extract()
+        # numpy array 作製
+        odds_arr = np.zeros((50, 4))
+        for i, num in enumerate(odds_num):
+            if re.match(r"\d\-\d\-\d", num) is not None and i <= 49:
+                # それぞれ代入
+                first, second, third = num.split('-')
+                odds_arr[i, 0] = first
+                odds_arr[i, 1] = second
+                odds_arr[i, 2] = third
+                odds_arr[i, 3] = odds[i]
+
+            else:
+                break
+
+        odds_table = pd.DataFrame(odds_arr)
+        odds_table.columns = ['first', 'second', 'third', 'odds']
+        odds_table = odds_table.astype(
+            {'first': int, 'second': int, 'third': int}
+        )
+        item['odds_table'] = odds_table
